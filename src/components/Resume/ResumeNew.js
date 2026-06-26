e3bf9f2ce0eacf39f1aad32678b8bfba585b68db
Shivam Kumar <shivam.saha06@gmail.com>
2026-06-26 16:22:32 +0530

    resume changed

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/RESUME_SHIVAM_KUMAR_JUN.pdf";
+import pdf from "../../Assets/../Assets/RESUME_SHIVAM_KUMAR_JULY.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
